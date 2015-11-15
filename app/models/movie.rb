556e28a9d74446b3e5eae230b5a47b2df3ac3a85
Ben Wong <bwong199@my.bcit.ca>
2015-11-14 23:48:56 -0700

    init

---
+++ app/models/movie.rb
@@ -0,0 +1,7 @@
+class Movie < ActiveRecord::Base
+	searchkick
+	belongs_to :user
+	has_many :reviews
+	has_attached_file :image, styles: { medium: "400x600#" }
+  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+end
